0720cabe34b1dd75d0832c2f6842b00b012a701d
rakibprog <contact.engrakib@gmail.com>
2025-06-21 02:11:26 +0600

    casting

---
+++ casting.py
@@ -0,0 +1,24 @@
+#Python casting
+
+#integers
+num = int('10')
+num1 = int(12)
+print(num)
+print(num1)
+
+#Float
+
+num = float(10.1)
+num1 = float('20')
+print(num)
+print(num1)
+
+#string
+
+name = str(10)
+nick_name = str("rocky")
+print(type(name))
+print(type(nick_name))
+
+
+
