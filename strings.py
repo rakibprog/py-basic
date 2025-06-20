a21277c6335a6a5b2f0300b11744fd8847e838ab
rakibprog <contact.engrakib@gmail.com>
2025-06-21 02:24:48 +0600

    multiline string

---
+++ strings.py
@@ -0,0 +1,8 @@
+# Multiline string
+
+story = """My name is a rocky, i am 27 , I have a son ,
+  my son name is Ebaad Ahmehd,
+  he is a good boy, her age is 8 months, 
+  he eats milk, vegetable, khicuri and banana"""
+
+print(story)
