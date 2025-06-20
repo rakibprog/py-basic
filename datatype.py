afb8ff3c6d0b8f3c0f09387f2a8dc914dcfbf49e
rakibprog <contact.engrakib@gmail.com>
2025-06-21 02:02:13 +0600

    random number generate

--- datatype.py
+++ datatype.py
@@ -1,3 +1,4 @@
+
 #Python Number
 x = 1 #int
 y = 100.1 #float
@@ -6,4 +7,33 @@ z = 10j #complex
 print(type(x))
 print(type(y))
 print(type(z))
+
+x = 5 + 5j
+print(x)
+
+
+# type conversation
+
+q = 10
+w = 5.0
+E = 2j
+
+a = float(q)
+b = int(w)
+c = complex(q)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+
+
+# Random number generate
+import  random
+
+print(random.randrange(0,10))
+
+
 
