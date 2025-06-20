ec741f6a584b6d0dcabfed0b603e7bb8dd1261c0
rakibprog <contact.engrakib@gmail.com>
2025-06-20 22:45:28 +0600

    Update variable

--- variable.py
+++ variable.py
@@ -5,3 +5,23 @@ y = 'Ebbad'
 print(x)
 print(y)
 
+#Variable do not need to be declared
+
+r = 10
+r = 'fatema'
+
+print(r)
+
+#Casting
+
+z = int(10)
+n = str(10)
+m = float(10)
+print(z)
+print(n)
+print(m)
+
+#Get the type
+
+a = 10
+print(type(a))
