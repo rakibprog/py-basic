047e8397a083bf3f705c52f13b2b69dc821eaf72
rakibprog <contact.engrakib@gmail.com>
2025-06-22 23:39:21 +0600

    split string

---
+++ modifyString.py
@@ -0,0 +1,30 @@
+#Upper case
+
+name = "my name is rocky"
+print(name.upper())
+
+
+#Lower case
+
+name = "MY NAME IS ROCKY"
+print(name.lower())
+
+
+
+# Remove  Whitespace
+
+a = " Hello, World! "
+print(a.strip())
+
+#Replace string
+
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+a = "HELLO, WORLD"
+print(a.replace("H", "R"))
+
+# Split String
+
+a = "Hello, World"
+print(a.split(","))
