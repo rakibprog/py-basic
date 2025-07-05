58de093e92f131a9b7b7ad03ac5fcc105ccd3153
rakibprog <contact.engrakib@gmail.com>
2025-07-05 22:33:27 +0600

    boolean

---
+++ boolean.py
@@ -0,0 +1,24 @@
+#Evaluate a string and a number:
+
+print(bool("Hello"))
+print(bool(15))
+
+
+#Evaluate values and variable
+
+a = 'hello'
+b = 10
+
+print(bool(a))
+print(bool(b))
+
+
+#Some variable are false
+
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool([]))
+print(bool({}))
+print(bool())
