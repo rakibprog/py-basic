a6c8acb1230752eeaee2609eab12d3c1f309e78d
rakibprog <contact.engrakib@gmail.com>
2025-07-12 01:26:58 +0600

    operator

---
+++ operator.py
@@ -0,0 +1,19 @@
+# Exponentiation
+x = 2
+y = 3
+print(x**y)
+
+#Floor Division
+x = 11
+y = 2
+print(x//y)
+
+
+#Membership operator
+
+x = ["apple", "banna"]
+print("banna" in x )
+
+print("rocky" not in x )
+
+
