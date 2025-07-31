334224147e9103933cd37f0f2bb9927af5dab5ab
rakibprog <contact.engrakib@gmail.com>
2025-07-31 22:25:04 +0600

    list terms

---
+++ Access_list_items.py
@@ -0,0 +1,19 @@
+#Change
+thislist        = ["Apple","banana","cherry"]
+thislist[1]     = "blackcurrant"
+print(thislist)
+
+
+#Change the second value replace it with two new values
+thislist  = ['aplle','banna','cherry']
+thislist[1:3] =["blackcurrent","jackfruit"]
+print(thislist)
+
+
+#Insert item
+thislist = ["apple","cherry","banna"]
+thislist.insert(2,"watermelon")
+print(thislist)
+
+
+
