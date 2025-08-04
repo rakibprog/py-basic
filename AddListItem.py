53a311043979f3dec2e212612cfc24b3716607b4
rakibprog <contact.engrakib@gmail.com>
2025-08-05 00:35:25 +0600

    remove list

---
+++ AddListItem.py
@@ -0,0 +1,25 @@
+#Append item
+thislist = ["apple","jackfruit","orange"]
+thislist.append("mango")
+print(thislist)
+
+#Insert Item
+
+thislist = ["tomaot","papaya","Lemon"]
+thislist.insert(1,'Cucumber')
+print(thislist)
+
+
+#extendlist
+
+listone = ['rocky','rakib','rabbi','fatema','ebbad']
+listtwo = ['Oli','samir','monira','Shahinur','easmin','salma','shajahan']
+
+listone.extend(listtwo)
+print(listone)
+
+
+thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
+print(thislist)
---
+++ Remove.py
@@ -0,0 +1,15 @@
+#Remove list
+
+thislist = ['Apple','Banna','Orange']
+
+thislist.remove('Banna')
+print(thislist)
+
+# Remove the first occurrence  of banna
+
+thislist = ['Apple','banna','orage','banna']
+
+thislist.remove('banna')
+print(thislist)
+
+
