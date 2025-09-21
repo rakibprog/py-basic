8ae1fcbf2cef17d32e23386befa7f21f2bbb4bb0
rakibprog <contact.engrakib@gmail.com>
2025-09-21 20:18:20 +0600

    list comprehension

---
+++ ListComprehension.py
@@ -0,0 +1,9 @@
+
+vegetable = ['tomato','onion','Lemon','Avocado']
+nobowel = [ ]
+
+for vegetables in vegetable:
+    if "o" in vegetables:
+        nobowel.append(vegetables)
+        print(nobowel)
+
