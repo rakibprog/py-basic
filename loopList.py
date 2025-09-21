8e9ec7bf5b490216bbfde72d09254249897dea63
rakibprog <contact.engrakib@gmail.com>
2025-09-21 19:52:03 +0600

    loop list

---
+++ loopList.py
@@ -0,0 +1,21 @@
+# Print all item in the list one by one
+from PyBasic.lists import vegetables
+
+thislist = ['apple','banna','cherry']
+for x in thislist:
+    print(x)
+
+#print all the item in the list one by one
+
+language = ['Bangla','English','Arabic']
+
+for languages in language:
+    print(languages)
+
+#Loop through the index number
+
+vegetables = ['tomato','onion','cucumber']
+
+for vegetable in range(len(vegetables)):
+    print(vegetable)
+
